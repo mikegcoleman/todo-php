c6fc2267a7796b127717ad2c252205d3b2990676
Mike Coleman <mikegcoleman@gmail.com>
2018-09-24 09:26:12 -0700

    reformatted install page

--- install.php
+++ install.php
@@ -1,22 +1,22 @@
-<?php
 
-/**
- * Open a connection via PDO to create a
- * new database and table with structure.
- *
- */
-echo "creating database";
-echo "<br>";
 
-require "../configs/config.php";
+<?php require("./templates/header.php"); ?>
 
-try {
-    echo "<br> Connecting to " . $host . " as " . $username . " with password " . $password . "<p>";
-	$connection = new PDO("mysql:host=$host", $username, $password, $options);
-	$sql = file_get_contents("data/init.sql");
-	$connection->exec($sql);
-	
-	echo "Database and table users created successfully.";
-} catch(PDOException $error) {
-	echo "ERROR!! - " . $sql . "<br>" . $error->getMessage();
-}
+	<h2>Creating Database</h2> <br>
+
+	<?php
+	require "../configs/config.php";
+
+	try {
+		echo "<br> Connecting to " . $host . " as " . $username . " with password " . $password . "<p>";
+		$connection = new PDO("mysql:host=$host", $username, $password, $options);
+		$sql = file_get_contents("data/init.sql");
+		$connection->exec($sql);
+		
+		echo "Database and table created successfully.";
+	} catch(PDOException $error) {
+		echo "ERROR!! - " . $sql . "<br>" . $error->getMessage();
+	}
+	?>
+
+<?php require("./templates/footer.php"); ?>
