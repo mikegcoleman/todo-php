89d9af1a24f51e3dba416aa2cd88ade69326bd23
Mike Coleman <mikegcoleman@gmail.com>
2018-12-10 21:24:11 -0800

    Added database host

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,8 @@
-<p class="font-weight-light font-italic" style="padding: 20px">Served by host: <?php echo gethostname(); ?></p>
+<p class="font-weight-light font-italic" style="padding: 20px">
+Front-end host: <?php echo gethostname(); ?><br>
+Database host: <?php echo $host; ?>
+
+</p>
 </body>
   <script src="./js/jquery.min.js"></script>
   <script src="./js/bootstrap.min.js"></script>
