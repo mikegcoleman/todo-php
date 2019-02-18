db634d7c7174c5be48d1030114d5e6118153c4f4
Mike Coleman <mikegcoleman@gmail.com>
2019-02-18 15:25:02 -0800

    changed location of config file

--- config.php
+++ config.php
@@ -2,6 +2,12 @@
   $host       = getenv('HOST');
   $username   = getenv('USER');
   $password   = getenv('PASSWORD');
+  echo "--------------";
+  echo "Environment variables: <br>";
+  echo "Host: " . $host . "<br";
+  echo "User: " . $username . "<br";
+  echo "Password: " . $password . "<br";
+
   $dsn        = "mysql:host=$host;dbname=tasks";
   $options    = array(
                   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
